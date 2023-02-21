31c1bbaa968802723772a2042b538526b2ca9da0
Abhijit <abhijitkumbhar924@gmail.com>
2023-02-21 15:10:59 +0530

    Updates

---
+++ precache-manifest.f11d2a775e57490840edb444bbe96cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2078ff575b548d65d811",
+    "url": "/ShoppingCart/static/css/main.bdb5939f.chunk.css"
+  },
+  {
+    "revision": "2078ff575b548d65d811",
+    "url": "/ShoppingCart/static/js/main.2078ff57.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/ShoppingCart/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/ShoppingCart/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "e123b3761f665870c38e",
+    "url": "/ShoppingCart/static/js/runtime~main.e123b376.js"
+  },
+  {
+    "revision": "4c10ef17a239d4294be6a054dcb7ead7",
+    "url": "/ShoppingCart/index.html"
+  }
+];
